96a8d4e54eb06ef08638591601f62e7fe60f8fa8
Akter H Sohag <akter@cefalo.com>
2018-08-28 20:12:59 +0600

    add commander

--- upstack-cli/index.js
+++ upstack-cli/index.js
@@ -1,3 +1,22 @@
 #!/usr/bin/env node
 
-console.log('upstack')
+/*const chalk = require('chalk')
+const figlet = require('figlet')
+
+console.log(
+  chalk.blue(
+    figlet.textSync('upstack', {horizontalLayout: 'full'})
+  )
+);*/
+
+const commander = require('commander')
+
+commander
+  .version(require('./package').version)
+  .usage('<command> [options]')
+
+commander
+  .option('--no-sauce', 'Remove sauce')
+  .parse(process.argv);
+
+console.log(`you ordered a pizza ${commander.sauce ? 'with' : 'without'} sauce`);
