9c9c312b39f4a8f412dd4315ea54558e524bc2a2
Akter H Sohag <akter@cefalo.com>
2018-08-29 18:55:40 +0600

    ddd

--- upstack-cli/index.js
+++ upstack-cli/index.js
@@ -9,6 +9,9 @@ console.log(
   )
 );*/
 
+
+const path = require('path')
+const fs = require('fs')
 const commander = require('commander')
 
 commander
@@ -16,7 +19,62 @@ commander
   .usage('<command> [options]')
 
 commander
-  .option('--no-sauce', 'Remove sauce')
-  .parse(process.argv);
+  .command('link <packagePath>')
+  .option('-f, --force', 'Create symbolic links forcefully.')
+  .option('-d, --dev-dependency', 'Add as devDependency')
+  .action((packagePath, cmd) => {
+    validateUpstackProject()
+    let pkgAbsPath = path.resolve(packagePath)
 
-console.log(`you ordered a pizza ${commander.sauce ? 'with' : 'without'} sauce`);
+    if (!fs.existsSync(pkgAbsPath + '/package.json')) {
+      throw new Error(`Invalid package in path: ${pkgAbsPath}/package.json`)
+    }
+
+    if (!require(pkgAbsPath + '/package').name) {
+      throw new Error('No package name found.')
+    }
+
+    let modules_dirs = [
+      `${process.cwd()}/linked_modules`,
+      `${process.cwd()}/node_modules`
+    ]
+    modules_dirs.forEach((dir) => {
+      mkdirSyncRecursive(dir)
+      let link = `${dir}/${path.basename(pkgAbsPath)}`
+      try {
+        if (cmd.force) {
+          if (fs.readlinkSync(link)) {
+            fs.unlinkSync(link)
+          } else if (fs.existsSync(link)) {
+            fs.rmdirSync(link)
+          }
+        }
+      } catch (e) {
+        console.log(e)
+      }
+      fs.symlinkSync(pkgAbsPath, link)
+    })
+    
+    let packageInfo = JSON.parse(fs.readFileSync(process.cwd() + '/package.json'))
+    packageInfo.dependencies = packageInfo.dependencies || {}
+    packageInfo.dependencies[require(pkgAbsPath + '/package.json').name] = require(pkgAbsPath + '/package.json').version;
+    fs.writeFileSync(process.cwd() + '/package.json', JSON.stringify(packageInfo))
+
+  })
+
+commander.parse(process.argv)
+
+function validateUpstackProject() {
+  if (!require(`${process.cwd()}/package`).upstack) {
+    throw new Error('This is not an upstack project')
+  }
+}
+
+function mkdirSyncRecursive(directory) {
+  let path = directory.replace(/\/$/, '').split('/');
+
+  for (let i = 1; i <= path.length; i++) {
+    let segment = path.slice(0, i).join('/');
+    segment.trim() && !fs.existsSync(segment) ? fs.mkdirSync(segment) : null;
+  }
+}
